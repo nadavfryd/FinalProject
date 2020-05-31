6dcbcf89f50b86a52b4b3a19df1a45b2fb431e3c
nadav fryd <nadav.fryd@gmail.com>
2020-05-31 17:25:19 +0300

    end

--- DemoFormProject/views.py
+++ DemoFormProject/views.py
@@ -179,7 +179,7 @@ def DataQuery():
     """Renders the DataQuery page."""
     form = CountriesFormStructure(request.form)
 
-    #calling for the my data base and using only the columns I need
+    #calling for my data base and using only the columns I need
     df = pd.read_csv(path.join(path.dirname(__file__), "static\\Data\\API_SL.UEM.TOTL.ZS_DS2_en_csv_v2_887304.csv"), skiprows=4, usecols = ['Country Name','Country Code', 'Indicator Name', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'])
 
     #creating a group of all the countries
